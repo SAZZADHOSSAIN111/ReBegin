f1a83147822e147f15dbcd51bb46e70c936bb8cd
SAZZADHOSSAIN111 <mdshazzadhossain215@gmail.com>
2024-09-04 11:05:33 +0600

    commit variable

--- intro.php
+++ intro.php
@@ -1,6 +1,16 @@
-<?php
- echo "hello world";
- echo "hello world"; 
- echo "hello world"; 
- 
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "first echo  <br>";
+    echo "first echo  <br>"; 
+    echo "first echo  <br>"; 
+    ?>
+    
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,15 @@
+<?php
+$x = 4;
+echo $x;
+echo "<br>";
+$x = 10;
+echo $x;
+
+/*Rules for PHP variables:
+
+A variable starts with the $ sign, followed by the name of the variable
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive ($age and $AGE are two different variables)*/
+?>
