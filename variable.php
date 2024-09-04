4e794a48dfdbbcb674e8cfad6e04907344ef68b7
SAZZADHOSSAIN111 <mdshazzadhossain215@gmail.com>
2024-09-04 12:11:29 +0600

    var commit

--- variable.php
+++ variable.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 $x = 4;
 echo $x;
 echo "<br>";
@@ -13,17 +12,4 @@ A variable name must start with a letter or the underscore character
 A variable name cannot start with a number
 A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
 Variable names are case-sensitive ($age and $AGE are two different variables)*/
-=======
-$x = 5;  // this is variable. and in php varible create with "$" sightn and after $ sign the words are the name of the variable and( variables ar case sensitive)
-
-echo $x; 
-
-/*Rules for PHP variables:
-
-    A variable starts with the $ sign, followed by the name of the variable
-    A variable name must start with a letter or the underscore character
-    A variable name cannot start with a number
-    A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
-    Variable names are case-sensitive ($age and $AGE are two different variables)*/
->>>>>>> 526027beb35a59847c8d5eef33d9e05817d14fdf
 ?>
