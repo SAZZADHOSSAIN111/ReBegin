cdd08b9060770c99f8f772d0339159fa9f39e762
SAZZADHOSSAIN111 <mdshazzadhossain215@gmail.com>
2024-09-05 19:45:15 +0600

    string modify concatinate

--- datatypes.php
+++ datatypes.php
@@ -36,5 +36,47 @@ $x = Null;
 var_dump ($x);
 echo "<br>";
 
+$x = strlen("The PHP strlen() function returns the length of a string.");
+echo  $x;
+echo "<br>";
+
+$x = str_word_count("The PHP str_word_count() function counts the number of words in a string.");
+echo  $x;
+echo "<br>";
+
+echo strpos("The PHP strpos() function searches for a specific text within a string." , "within");
+echo "<br>";
+
+$x = "The PHP strlen() function returns the length of a string.";
+echo  strtoupper($x);
+echo "<br>";
+
+$x = "The PHP strlen() function returns the length of a string.";
+echo  strtolower($x);
+echo "<br>";
+
+$x = "The PHP strlen() function returns the length of a string.";
+echo  str_replace("strlen()", "str_replace" ,$x);
+echo "<br>";
+
+$x = "The PHP strlen() function returns the length of a string.";
+echo  strrev($x);
+echo "<br>";
+
+$x = "The PHP strlen() function returns the length of a string.";
+$y = explode(" ", "$x");
+print_r($y);
+echo "<br>";
+
+$x = "abdul";
+$y = "hamid";
+$z = $x . " " . $y;
+echo $z;
+echo "<br>";
+
+$x = "4";
+$y = "6";
+$z = $x .  $y;
+echo $z;
 
 ?>
