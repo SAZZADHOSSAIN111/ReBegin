828e834cb7701d74d3129bbfc650c88b0dece3a1
SAZZADHOSSAIN111 <mdshazzadhossain215@gmail.com>
2024-09-05 18:58:21 +0600

    datatype

---
+++ datatypes.php
@@ -0,0 +1,40 @@
+<?php
+ //PHP Data Types
+    //  String
+    // Integer
+    // Float (floating point numbers - also called double)
+    // Boolean
+    // Array
+    // Object
+    // NULL
+    // Resource
+
+// Getting the Data Type
+    //  You can get the data type of any object by using the var_dump() function.
+
+$x =50000;
+var_dump ($x);
+echo "<br>";
+
+$x ="this is an string";
+var_dump ($x);
+echo "<br>";
+
+$x =10.123;
+var_dump ($x);
+echo "<br>";
+
+$x =true;
+var_dump ($x);
+echo "<br>";
+
+$x = array("car","dog","cat");
+var_dump ($x);
+echo "<br>";
+
+$x = Null;
+var_dump ($x);
+echo "<br>";
+
+
+?>
