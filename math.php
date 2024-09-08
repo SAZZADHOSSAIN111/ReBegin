e19241f6bc1c20b2f9c4fabc78b10dbe94949e44
SAZZADHOSSAIN111 <mdshazzadhossain215@gmail.com>
2024-09-08 22:11:27 +0600

    math practice

---
+++ math.php
@@ -0,0 +1,21 @@
+<?php
+echo(pi());
+echo "<br>";
+
+echo(min(122, 100, 134, 544, -12, 12, 65, 0,));
+echo "<br>";
+echo(max(122, 100, 134, 544, -12, 12, 65, 0,));
+echo "<br>";
+echo (abs(-6.323)); //The abs() function returns the absolute (positive) value of a number:
+echo "<br>";
+echo(sqrt(81));//The sqrt() function returns the square root of a number:
+echo "<br>";
+echo(round(0.60));
+echo "<br>";
+echo(round(0.40));
+echo "<br>";
+echo(round(34.40));
+echo "<br>";
+echo(rand(10,102));
+
+?>
