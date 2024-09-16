f25c7751a1bc05ce310fe2498a2134bb8eec6ffb
SAZZADHOSSAIN111 <mdshazzadhossain215@gmail.com>
2024-09-16 23:57:17 +0600

    if

---
+++ if-else.php
@@ -0,0 +1,13 @@
+<?php
+/*In PHP we have the following conditional statements:
+
+if statement - executes some code if one condition is true
+if...else statement - executes some code if a condition is true and another code if that condition is false
+if...elseif...else statement - executes different codes for more than two conditions
+switch statement - selects one of many blocks of code to be executed
+*/
+
+if (5>3){
+    echo "this is great";
+}
+?>
